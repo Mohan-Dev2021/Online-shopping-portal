657b3a87a8f372bf11e4a16a459eba432174721a
sivaranjani2311 <s2311ranjani@gmail.com>
2023-08-30 10:12:25 +0530

    [To get a products api for id and productId]

--- Online-shopping-platform/src/main/java/com/online/shop/controller/ProductController.java
+++ Online-shopping-platform/src/main/java/com/online/shop/controller/ProductController.java
@@ -1,8 +1,16 @@
 package com.online.shop.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.online.shop.dto.ProductDto;
+import com.online.shop.repository.ProductRepo;
 import com.online.shop.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,4 +21,17 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 
 	private final ProductService productService;
+
+	private final ProductRepo productRepo;
+
+	@GetMapping("/product/{id}")
+	public ResponseEntity<ProductDto> getProductById(@PathVariable String id) {
+		return ResponseEntity.status(HttpStatus.OK).body(productService.getProductById(id));
+
+	}
+
+	@GetMapping("/product/{productId}")
+	public ResponseEntity<ProductDto> getProductByProductId(@PathVariable String productId) {
+		return ResponseEntity.status(HttpStatus.OK).body(productService.getProductByProductId(productId));
+	}
 }
--- Online-shopping-platform/src/main/java/com/online/shop/service/ProductService.java
+++ Online-shopping-platform/src/main/java/com/online/shop/service/ProductService.java
@@ -1,5 +1,15 @@
 package com.online.shop.service;
 
+import java.util.Optional;
+
+import com.online.shop.dto.CustomerDto;
+import com.online.shop.dto.ProductDto;
+
 public interface ProductService {
+	
+	public ProductDto getProductByProductId(String productId );
+	
+	public ProductDto getProductById(String id);
+
 
 }
